aec8948ddd7839a5e224d60ef02327f8a281a64c
Issam Boubcher <boubcher.issam@gmail.com>
2022-08-25 22:51:41 +0100

    more removed deps

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,16 +8,16 @@
 
 
  require('./bootstrap');
- require('echarts');
- 
+//  require('echarts');
+
 //  require('apexcharts');
 //  import ApexCharts from 'apexcharts';
 
- 
+
 //  require('./AccountTypeSelection'); //For /equipe/ajout-membre
- 
+
 //  require('./searchTeamTable');
- 
+
 //  require('./FilterByAccountType');  // for /equipe Filter team table by account type
 
  // import ReactDOM from "react-dom";
@@ -28,17 +28,17 @@
  // import Login from "./components/Login";
  // import Register from "./components/Register";
  // import ManagerNavbar from "./components/ManagerNavbar";
- 
+
  /**
   * Next, we will create a fresh React component instance and attach it to
   * the page. Then, you may begin adding components to this application
   * or customize the JavaScript scaffolding to fit your unique needs.
   */
- 
+
  // require('./components/Example');
- 
- 
- 
+
+
+
  // if (document.getElementById("example")) {
  //     ReactDOM.render(<Example />, document.getElementById("example"));
  // }
