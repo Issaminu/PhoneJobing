25332c84e9bb813ddad6412a795ead1d6e4d7f58
Issam Boubcher <boubcher.issam@gmail.com>
2022-08-26 00:17:33 +0100

    reducing assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@
 
 
 
- require('./bootstrap');
+//  require('./bootstrap');
 //  require('echarts');
 
 //  require('apexcharts');
@@ -29,11 +29,11 @@
  // import Register from "./components/Register";
  // import ManagerNavbar from "./components/ManagerNavbar";
 
- /**
-  * Next, we will create a fresh React component instance and attach it to
-  * the page. Then, you may begin adding components to this application
-  * or customize the JavaScript scaffolding to fit your unique needs.
-  */
+/**
+ * Next, we will create a fresh React component instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
 
  // require('./components/Example');
 
