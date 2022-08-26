f2fdb51f2ec91ea761eca4b147bc330cc971d910
Issam Boubcher <boubcher.issam@gmail.com>
2022-08-26 01:42:18 +0100

    ffs

--- resources/views/layouts/Scripts/listAllScripts-body.blade.php
+++ resources/views/layouts/Scripts/listAllScripts-body.blade.php
@@ -5,7 +5,7 @@
                 style="margin-bottom: 16px; min-width: 30%;max-width: 30%; height:fit-content; margin-bottom: calc(10%/6); margin-left: calc(10%/6); margin-right: calc(10%/6);">
 
                 <div class="card-body pb-5" style="display: flex; flex-direction: column;align-items: center;">
-                    <img src="{{ asset('script-icon.jpg') }}" alt="script icon" width="140" height="150" />
+                    <img src="{{ asset('images/script-icon.jpg') }}" alt="script icon" width="140" height="150" />
                     <h4 class="h2 mt-3" style="color: #f0bc74; text-shadow: 1px 1px 2px #be7f26;
                 ">
                         {{ ucwords($script->name) }}</h4>
